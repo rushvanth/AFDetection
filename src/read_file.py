9a1ef208c068a6278c64a3514e57adfd217e05c6
rushvanth <rushvanth@gmail.com>
2021-12-30 17:29:55 +0100

    Changed data read path

--- src/read_file.py
+++ src/read_file.py
@@ -7,12 +7,7 @@ from sklearn import metrics
 # Read file from parent directory
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
-file_name = 'Preprocessed_AFData.xlsx'
-file_path = os.path.join(parent_dir, file_name)
+file_path = 'data/Preprocessed_AFData.xlsx'
+relative_path = os.path.join(parent_dir, file_path)
 
-data = pd.read_excel(file_path)
-print(data.head())
-
-af_episodes = list(data['Control'])
-count_1s = [i for i in af_episodes if i == 1]
-print(len(count_1s))
+data = pd.read_excel(relative_path)
