863b65babaed0cba705bc332e0b130a450e2891a
rushvanth <rushvanth@gmail.com>
2022-01-28 22:57:48 +0100

    Made pretty print statements

--- src/svm.py
+++ src/svm.py
@@ -8,14 +8,14 @@ def svm(X, y):
     # Split data into training and testing sets
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
     # Use SVM to predict the outcome
-    print("Training Support Vector Machine...")
+    print("Training Support Vector Machine...\n")
     svm = SVC(kernel='rbf', random_state=101)
     svm.fit(x_train, y_train)
     y_pred = svm.predict(x_test)
     print("Support Vector Machine Results\n" + "-" * 50 + "\n")
-    print('Accuracy of SVM: {:.4F}'.format(metrics.accuracy_score(y_test, y_pred) * 100))
+    print('Accuracy of SVM: {:.4F} \n'.format(metrics.accuracy_score(y_test, y_pred) * 100))
     print('Confusion Matrix (SVM): \n', metrics.confusion_matrix(y_test, y_pred))
-    print('Area under curve (SVM): {:.4F}'.format(metrics.roc_auc_score(y_test, y_pred)))
+    print('Area under curve (SVM): {:.4F} \n'.format(metrics.roc_auc_score(y_test, y_pred)))
     print(metrics.classification_report(y_test, y_pred))
     print("-" * 50 + "\n")
     return {'model': svm, 'x_train': x_train, 'x_test': x_test, 'y_train': y_train, 'y_test': y_test, 'y_pred': y_pred}
