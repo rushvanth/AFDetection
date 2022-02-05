962463d9668cf382ed42ac365926d45e2c498a4f
rushvanth <rushvanth@gmail.com>
2022-02-05 18:32:06 +0100

    Updated Notebook

--- src/main.py
+++ src/main.py
@@ -12,25 +12,25 @@ af_data = load_data.gen_metadata()
 X = af_data.drop(['Control'], axis=1)
 y = af_data['Control']
 
-# Decision Tree Classifier
-dt_results = decision_tree.decision_tree(X, y)
-# Visualize results
-visualize.visualize_results(dt_results, "decision_tree")
+# # Decision Tree Classifier
+# dt_results = decision_tree.decision_tree(X, y)
+# # Visualize results
+# visualize.visualize_results(dt_results, "decision_tree")
 
-# KNN Classifier
-knn.knn(X, y)
-# Visualize results
-#visualize.visualize_results(knn_results, "knn")
+# # KNN Classifier
+# knn.knn(X, y)
+# # Visualize results
+# #visualize.visualize_results(knn_results, "knn")
 
-# Random Forest Classifier
-rf_results = random_forest.random_forest(X, y)
-# Visualize results
-visualize.visualize_results(rf_results, "random_forest")
+# # Random Forest Classifier
+# rf_results = random_forest.random_forest(X, y)
+# # Visualize results
+# visualize.visualize_results(rf_results, "random_forest")
 
-nonlinear_svm_results = svm.svm(X, y, kernel='rbf')
-visualize.visualize_results(nonlinear_svm_results, "nonlinear_svm")
+# nonlinear_svm_results = svm.svm(X, y, kernel='rbf')
+# visualize.visualize_results(nonlinear_svm_results, "nonlinear_svm")
 
-# Support Vector Machine
-svm_results = svm.svm(X, y)
-# Visualize results
-visualize.visualize_results(svm_results, "linear_svm")
+# # Support Vector Machine
+# svm_results = svm.svm(X, y)
+# # Visualize results
+# visualize.visualize_results(svm_results, "linear_svm")
